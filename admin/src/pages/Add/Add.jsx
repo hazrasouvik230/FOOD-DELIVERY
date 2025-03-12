93b1ce1fbad3230515a1888778be4297bf19d504
SOUVIK HAZRA <165355332+hazrasouvik230@users.noreply.github.com>
2025-03-12 10:34:05 +0530

    Update Add.jsx

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -108,7 +108,7 @@ const Add = ({ url }) => {
               value={data.price}
               type="Number"
               name="price"
-              placeholder="₹20"
+              placeholder="₹ 20"
             />
           </div>
         </div>
