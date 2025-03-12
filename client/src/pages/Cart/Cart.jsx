623bfc18426eb315a365de2332643662d677267a
SOUVIK HAZRA <165355332+hazrasouvik230@users.noreply.github.com>
2025-03-12 10:43:50 +0530

    Update Cart.jsx

--- client/src/pages/Cart/Cart.jsx
+++ client/src/pages/Cart/Cart.jsx
@@ -27,7 +27,7 @@ const Cart = () => {
                 <div className="cart-items-title cart-items-item">
                   <img src={url + "/images/" + item.image} alt="" />
                   <p>{item.name}</p>
-                  <p>$ {item.price}</p>
+                  <p>₹ {item.price}</p>
                   <p>{cartItems[item._id]}</p>
                   <p>₹ {item.price * cartItems[item._id]}</p>
                   <p className="cross" onClick={() => removeFromCart(item._id)}>
